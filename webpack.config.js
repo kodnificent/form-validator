de1a76f0b9093d3b8376e8a4b69fcee220669a4b
kodnificent <mbamaravictor@gmail.com>
2020-08-30 19:28:32 +0100

    setup linting

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,40 @@
 const path = require('path');
-const isProduction = process.env.NODE_ENV === 'production' || process.argv.includes('-p')
+
+const isProduction = process.env.NODE_ENV === 'production' || process.argv.includes('-p');
 
 module.exports = {
-    mode: isProduction ? 'production' : 'development',
-    entry : './src/index.js',
-    output : {
-        filename: 'index.js',
-        path: path.resolve(__dirname, 'dist')
-    }
+  mode: isProduction ? 'production' : 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'index.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        enforce: 'pre',
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'eslint-loader',
+          options: {
+            cache: true,
+            failOnError: true,
+          },
+        },
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            cacheDirectory: true,
+            presets: ['@babel/preset-env'],
+            plugins: ['@babel/plugin-transform-runtime', '@babel/plugin-proposal-object-rest-spread'],
+          },
+        },
+      },
+    ],
+  },
 };
